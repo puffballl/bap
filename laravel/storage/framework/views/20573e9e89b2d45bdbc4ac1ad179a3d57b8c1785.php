22860cc8862e55d8ec28e05665325a51e9be6b0e
Quinten <31628811+puffballl@users.noreply.github.com>
2018-11-09 21:07:19 +0100

    Add files via upload

---
+++ laravel/storage/framework/views/20573e9e89b2d45bdbc4ac1ad179a3d57b8c1785.php
@@ -0,0 +1,8 @@
+ends('layout.master')
+
+<?php $__env->startSection('header'); ?>
+    <h2>Atrtikel <?php echo e($article_id); ?></h2>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi corporis distinctio, nihil officia quae ut voluptate! Doloribus iure laudantium vitae.
+    </p>
+<?php $__env->stopSection(); ?>
---
+++ laravel/storage/framework/views/46099bded7297d472c5fd51c43bd0895fa796df9.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('header'); ?>
+    <h2>header yeeters</h2>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layout.master', \Illuminate\Support\Arr::except(get_defined_vars(), array('__data', '__path')))->render(); ?>
